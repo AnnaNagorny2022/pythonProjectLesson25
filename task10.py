ab177f36cdba01946d28c612ca4259601927e6b9
AnnaNagorny2022 <a.melyukh@mail.ru>
2023-04-18 21:38:16 +0300

    ....

---
+++ task10.py
@@ -0,0 +1,7 @@
+empty_dict1 = {}
+empty_dict2 = dict()
+# d1 = dict([(1, 0), (2, 0), (3, 0), (4, 0), (5, 0)])
+# d1 = dict.fromkeys("abcde")
+# d1 = dict.fromkeys([1, 2, 3, 4, 5])
+d1 = dict.fromkeys("abcde", 0)
+print(d1)
